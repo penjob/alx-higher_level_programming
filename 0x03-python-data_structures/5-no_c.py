77223be8d69e54907b8f6ec9c9c3fe23b931f814
penjob <penjob07gmail.com>
2024-03-19 16:11:41 -0700

    hh

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     ret = ""
     for i in range(len(my_string)):
         if (my_string[i] != 'c' and my_string[i] != 'C'):
-            ret =+ my_string[i]
+            ret += my_string[i]
     return ret
