06129de6e9d6c0fadf4f62c0a0f45cea9c88d039
penjob <penjob07gmail.com>
2024-03-19 18:07:47 -0700

    hh

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,5 +7,5 @@ def print_matrix_integer(matrix=[[]]):
             print()
         for i in range(len(submatrix)):
             print("{:d}".format(submatrix[i]),
-                    end ="\n" if i is len(submatrix) - 1 else " ")
+                    end="\n" if i is len(submatrix) - 1 else " ")
 
