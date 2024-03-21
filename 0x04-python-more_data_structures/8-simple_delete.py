719c9689b4bbac390a81ae2345cbc15023488fe9
penjob <penjob07gmail.com>
2024-03-20 19:41:35 -0700

    hh

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
-    return a_dictionary
+    if a_dictionary.get(key) is not None:
+        del a_dictionary[key]
+        return a_dictionary
